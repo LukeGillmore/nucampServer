fe0b48b27524eae2ba1a980de79529cc2c5249db
Luke <lucasgillmore@yahoo.com>
2020-07-26 14:20:59 -0700

    Added: favorites

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '346781069671643',
+        clientSecret: 'b7926e32342ab3857f0b5e8eb4c639c5'
+    }
 }
